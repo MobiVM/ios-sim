e1916e0974a9272ee15d6e1c3e852d6858f64fa7
Niklas Therning <niklas@therning.org>
2014-03-17 10:08:50 +0100

    Fixed check for --unbuffered option.

--- Source/iPhoneSimulator.m
+++ Source/iPhoneSimulator.m
@@ -509,7 +509,7 @@ NSString* FindDeveloperDir() {
           i++;
           stderrPath = [[NSString stringWithUTF8String:argv[i]] expandPath];
           NSLog(@"stderrPath: %@", stderrPath);
-      } else if (strcmp(argv[i], "--unbuffered")) {
+      } else if (strcmp(argv[i], "--unbuffered") == 0) {
           setbuf(stdout, NULL);
       } else if (strcmp(argv[i], "--xctest") == 0) {
           i++;
